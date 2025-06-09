b933df5b0dabc367cbc0e3988219325f008ad1bf
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-06-10 00:50:55 +0200

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
+import CatalogPage from "./pages/CatalogPage/CatalogPage";
+import AutoDetailsPage from "./pages/AutoDetailsPage/AutoDetailsPage";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />}>
+          <Route path=":id" element={<AutoDetailsPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
