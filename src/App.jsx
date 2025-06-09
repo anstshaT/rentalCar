d55a10800c61dfe35552131909620b5031893d41
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-06-10 01:04:59 +0200

    Add basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import CatalogPage from "./pages/CatalogPage/CatalogPage";
