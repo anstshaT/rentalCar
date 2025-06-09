e3bf955f171ac2c1c9f2972c0877b7f18f8a3169
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-06-10 00:39:17 +0200

    Add vite files and home, catalog, details pages both .jsx and .css

---
+++ src/pages/AutoDetailsPage/AutoDetailsPage.jsx
@@ -0,0 +1,9 @@
+const AutoDetailsPage = () => {
+  return (
+    <div>
+      <h1>Auto details page</h1>
+    </div>
+  );
+};
+
+export default AutoDetailsPage;
---
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -0,0 +1,9 @@
+const CatalogPage = () => {
+  return (
+    <div>
+      <h1>Catalog page</h1>
+    </div>
+  );
+};
+
+export default CatalogPage;
