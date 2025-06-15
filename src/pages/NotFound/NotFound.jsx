5496635c223926447400d2c062a83c73e44f2add
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-06-15 17:27:07 +0200

    Add notFound Page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router";
+import s from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not found page</h1>
+    <div className={s.wrapper}>
+      <p className={s.title}>Lost in space?</p>
+      <p className={s.text}>
+        We couldn’t find the page you’re looking for. Maybe it got sucked into a
+        black hole.
+      </p>
+      <p className={s.text}>
+        Don’t worry,{" "}
+        <Link to="/" className={s.link}>
+          let’s get you back on track
+        </Link>
+      </p>
     </div>
   );
 };
